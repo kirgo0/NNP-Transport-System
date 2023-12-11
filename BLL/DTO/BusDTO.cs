552557908e77d9bc8cce34685c2d640e0aaff0c0
kirgo <kirgotop@gmail.com>
2023-12-11 18:13:25 +0200

    Added DTO for backlog story required entities

---
+++ BLL/DTO/BusDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class BusDTO
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public bool isAvailable { get; set; }
+
+        [Required]
+        public int maxSeatsAmount { get; set; }
+
+        [Required]
+        public int availableSeatsAmount { get; set; }
+
+        [Required]
+        public int trip_Id { get; set; }
+    }
+}
---
+++ BLL/DTO/ScheduleDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class ScheduleDTO
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public DateTime departureTime { get; set; }
+
+        [Required]
+        public DateTime arrivalTime { get; set; }
+
+        [Required]
+        public int trip_Id { get; set; }
+    }
+}
