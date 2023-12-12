fe734f23cb96982cb953bdabccbe340981a7536b
kirgo <kirgotop@gmail.com>
2023-12-12 15:14:59 +0200

    Added REST API Project & Trip controller

---
+++ NNP Transport System/Controllers/TripController.cs
@@ -0,0 +1,46 @@
+﻿using BLL.DTO;
+using BLL.Services.IService;
+using CCL.Security;
+using CCL.Security.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NNP_Transport_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripController : ControllerBase
+    {
+
+        private ITripService _tripService;
+
+        public TripController(ITripService tripService)
+        {
+            _tripService = tripService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _tripService.GetAllTrips();
+
+            if (result == null) return BadRequest();
+
+            return Ok(result);
+        }
+
+        // 2021-12-11 20:03:03
+
+        [HttpGet("ForAdmin")]
+        public IActionResult FindTripsBySchedule([FromQuery] DateTime departureTime, [FromQuery] DateTime arrivalTime)
+        {
+            SecurityContext.SetUser(new Admin(1,"Kirgo"));
+
+            var result = _tripService.FindTripsBySchedule(departureTime, arrivalTime);
+
+            if (result == null) return BadRequest();
+
+            return Ok(result);
+        }
+    }
+}
--- NNP Transport System/Program.cs
+++ NNP Transport System/Program.cs
@@ -1,4 +1,9 @@
+using BLL;
+using BLL.Services;
+using BLL.Services.IService;
 using DAL;
+using DAL.Repository;
+using DAL.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +20,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("MySQL"));
 });
 
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddScoped<IBusRepository, BusRepository>();
+builder.Services.AddScoped<IBusStopRepository, BusStopRepository>();
+builder.Services.AddScoped<ITripRepository, TripRepository>();
+builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
+
+builder.Services.AddScoped<ITripService, TripService>();
 
 var app = builder.Build();
 
